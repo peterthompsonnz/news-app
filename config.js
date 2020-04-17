560ce4d081233f347139d92d6af9dae96ac18e16
Joe Bloggs <you@example.com>
2020-04-17 20:32:57 +1200

    Changed http to https for some sources

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const feeds = {
   "New Zealand": `https://newsapi.org/v2/top-headlines?country=nz&apiKey=`,
-  Australia: `http://newsapi.org/v2/top-headlines?country=au&apiKey=`,
-  UK: `http://newsapi.org/v2/top-headlines?country=gb&apiKey=`,
-  USA: `http://newsapi.org/v2/top-headlines?country=us&apiKey=`,
+  Australia: `https://newsapi.org/v2/top-headlines?country=au&apiKey=`,
+  UK: `https://newsapi.org/v2/top-headlines?country=gb&apiKey=`,
+  USA: `https://newsapi.org/v2/top-headlines?country=us&apiKey=`,
   BBC: `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=`,
-  Singapore: `http://newsapi.org/v2/top-headlines?country=sg&apiKey=`,
+  Singapore: `https://newsapi.org/v2/top-headlines?country=sg&apiKey=`,
   Reuters: `https://newsapi.org/v2/top-headlines?sources=reuters&apiKey=`,
   "National Geographic": `https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=`,
 };
