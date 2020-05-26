4d251eff7f45556b5539ae4097a74ebce5cff00c
Joe Bloggs <you@example.com>
2020-05-26 21:36:40 +1200

    CORS issues: Added https://crossorigin.me/ to start of URLs

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const feeds = {
-  "New Zealand": `https://newsapi.org/v2/top-headlines?country=nz&apiKey=`,
-  Australia: `https://newsapi.org/v2/top-headlines?country=au&apiKey=`,
-  UK: `https://newsapi.org/v2/top-headlines?country=gb&apiKey=`,
-  USA: `https://newsapi.org/v2/top-headlines?country=us&apiKey=`,
+  "New Zealand": `https://crossorigin.me/https://newsapi.org/v2/top-headlines?country=nz&apiKey=`,
+  Australia: `https://crossorigin.me/https://newsapi.org/v2/top-headlines?country=au&apiKey=`,
+  UK: `https://crossorigin.me/https://newsapi.org/v2/top-headlines?country=gb&apiKey=`,
+  USA: `https://crossorigin.me/https://newsapi.org/v2/top-headlines?country=us&apiKey=`,
   BBC: `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=`,
-  Singapore: `https://newsapi.org/v2/top-headlines?country=sg&apiKey=`,
-  Reuters: `https://newsapi.org/v2/top-headlines?sources=reuters&apiKey=`,
-  "National Geographic": `https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=`,
+  Singapore: `https://crossorigin.me/https://newsapi.org/v2/top-headlines?country=sg&apiKey=`,
+  Reuters: `https://crossorigin.me/https://newsapi.org/v2/top-headlines?sources=reuters&apiKey=`,
+  "National Geographic": `https://crossorigin.me/https://newsapi.org/v2/top-headlines?sources=national-geographic&apiKey=`,
 };
 
 const apiKey = "027fe0ba771349fa87775b60c62ba426";
